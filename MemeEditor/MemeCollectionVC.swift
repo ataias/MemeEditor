4aa69a84637984d18fe49baed31a63a041087068
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-12-17 07:24:33 -0300

    Add flow layout to set collection cell size

--- MemeEditor/MemeCollectionVC.swift
+++ MemeEditor/MemeCollectionVC.swift
@@ -11,6 +11,7 @@ class MemeCollectionVC: UIViewController, UICollectionViewDelegate, UICollection
 
     var memes = [Meme]()
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return memes.count
@@ -29,6 +30,14 @@ class MemeCollectionVC: UIViewController, UICollectionViewDelegate, UICollection
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let space: CGFloat = 3.0
+        let dimension = (view.frame.size.width - (2 * space)) / 3.0
+
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
+
     }
 
     override func viewWillAppear(_ animated: Bool) {
